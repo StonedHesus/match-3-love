a537cb8ad4e5f49f769edab9d9be334caafcaeca
stonedhesus <ionescu_paul@outlook.com>
2022-12-24 22:14:31 +0100

    Concluded the issue.

--- main.lua
+++ main.lua
@@ -1,4 +1,12 @@
+require 'src.dependencies'
+
 function love.load()
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true,
+        canvas = true
+    })
 end
 
 function love.update(deltaTime)
